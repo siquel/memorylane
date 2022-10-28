180588c2d87a5c1d9529566c5cc58e725b0deb69
Jani Niemelä <jani@swappie.com>
2022-10-28 18:26:47 +0300

    Configure testing with Junit 5

---
+++ src/test/java/me/siquel/MainTest.java
@@ -0,0 +1,13 @@
+package me.siquel;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+  @Test
+  public void adding_1plus1_equals_two() {
+    assertEquals(2, 1 + 1);
+  }
+}
